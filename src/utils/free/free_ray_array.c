b4ab07a924fd843fb27c618fccf92fbd846e40fd
Kufato <axel.callet@hotmail.com>
2023-08-23 08:53:03 +0200

    Cleanning before push

--- src/utils/free/free_ray_array.c
+++
@@ -1,6 +0,0 @@
-#include "cub3d.h"
-
-void    free_ray_array(t_ray *ray_array)
-{
-    free(ray_array);
-}
--- src/utils/rgba_to_hex.c
+++
@@ -1,9 +0,0 @@
-#include "cub3d.h"
-
-int    rgba_to_hex(int r, int g, int b, int a)
-{
-    int    hex;
-
-    hex = ((r & 0xff) << 24) + ((g & 0xff) << 16) + ((b & 0xff) << 8) + (a & 0xff);
-    return (hex);
-}
